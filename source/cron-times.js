08b3bc3df423ad8fd70441e45faf290a7c19cc6b
nikistgar <nikistgar@gmail.com>
2023-09-20 18:59:29 +0300

    .env

--- source/cron-times.js
+++ source/cron-times.js
@@ -1,14 +1,15 @@
+require('dotenv').config();
 const CronJob = require('cron').CronJob;
 
 const { mondayembed, mondaypng, tuesdayembed, tuesdaypng,
         wednesdayembed, wednesdaypng, thursdayembed, thursdaypng,
-        fridayembed, fridaypng} = require('./messages')
+        fridayembed, fridaypng, kachembed, kachpng} = require('./messages')
 
 async function primarycronstart (client)
 {
     console.log('Cron detected');
     const monday = new CronJob ('0 0 0/1 * * 1', async function(monday) {
-        await client.channels.cache.get('1153261980656865340').messages.fetch('1153353857770999841')
+        await client.channels.cache.get(process.env.SCHOOL_SCHEDULE_CHANNEL).messages.fetch(process.env.SCHOOL_SCHEDULE_MESSAGE)
         .then(message =>{message.edit({ content: '',embeds: [mondayembed], files: [mondaypng] })});
     },
         null,
@@ -17,7 +18,7 @@ async function primarycronstart (client)
 
 
     const tuesday = new CronJob ('0 0 0/1 * * 2', async function(tuesday) {
-        await client.channels.cache.get('1153261980656865340').messages.fetch('1153353857770999841')
+        await client.channels.cache.get(process.env.SCHOOL_SCHEDULE_CHANNEL).messages.fetch(process.env.SCHOOL_SCHEDULE_MESSAGE)
         .then(message =>{message.edit({ content: '',embeds: [tuesdayembed], files: [tuesdaypng] })});
     },
         null,
@@ -25,7 +26,7 @@ async function primarycronstart (client)
         "Europe/Moscow");
 
     const wednesday = new CronJob ('0 0 0/1 * * 3', async function(wednesday) {
-        await client.channels.cache.get('1153261980656865340').messages.fetch('1153353857770999841')
+        await client.channels.cache.get(process.env.SCHOOL_SCHEDULE_CHANNEL).messages.fetch(process.env.SCHOOL_SCHEDULE_MESSAGE)
         .then(message =>{message.edit({ content: '',embeds: [wednesdayembed], files: [wednesdaypng] })});
     },
         null,
@@ -33,7 +34,7 @@ async function primarycronstart (client)
         "Europe/Moscow");
 
     const thursday = new CronJob ('0 0 0/1 * * 4', async function(thursday) {
-        await client.channels.cache.get('1153261980656865340').messages.fetch('1153353857770999841')
+        await client.channels.cache.get(process.env.SCHOOL_SCHEDULE_CHANNEL).messages.fetch(process.env.SCHOOL_SCHEDULE_MESSAGE)
         .then(message =>{message.edit({ content: '',embeds: [thursdayembed], files: [thursdaypng] })});
     },
         null,
@@ -41,7 +42,7 @@ async function primarycronstart (client)
         "Europe/Moscow");
 
     const friday = new CronJob ('0 0 0/1 * * 5', async function(friday) {
-        await client.channels.cache.get('1153261980656865340').messages.fetch('1153353857770999841')
+        await client.channels.cache.get(process.env.SCHOOL_SCHEDULE_CHANNEL).messages.fetch(process.env.SCHOOL_SCHEDULE_MESSAGE)
         .then(message =>{message.edit({ content: '',embeds: [fridayembed], files: [fridaypng] })});
     },
         null,
@@ -49,7 +50,7 @@ async function primarycronstart (client)
         "Europe/Moscow");
 
     const job = new CronJob('0 0 0 2/2 * *', function(gymcron) {
-            client.channels.cache.get('1025479339900403864')
+            client.channels.cache.get(process.env.PRIMARY_CHANNEL)
             .send({ embeds: [kachembed], files: [kachpng] });
         },
             null,
