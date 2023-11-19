de4e2bfdddb9af8e8b6390bd54b39a928e136071
nikistgar <nikistgar@gmail.com>
2023-11-19 14:45:23 +0300

    1.2

--- source/commands/gym.js
+++ source/commands/gym.js
@@ -16,27 +16,8 @@ module.exports = {
   
       collector.on('collect', async (interaction) => {
           await interaction.deferUpdate();
-          switch(interaction.customId){
-            case 'day1':
-                await interaction.message.edit({ embeds: [day1embed], components:[gymschedule, gymschedule2], files: [day1png] });
-                return;
-            case 'day2':
-                await interaction.message.edit({ embeds: [day2embed], components:[gymschedule, gymschedule2], files: [day2png] });
-                return;
-            case 'day3':
-                await interaction.message.edit({ embeds: [day3embed], components:[gymschedule, gymschedule2], files: [day3png] });
-                return;
-            case 'day4':
-                await interaction.message.edit({ embeds: [day4embed], components:[gymschedule, gymschedule2], files: [day4png] });
-                return;
-            case 'dayc':
-                await interaction.message.edit({ embeds: [daycembed], components:[gymschedule, gymschedule2], files: [daycpng] });
-                return;
-            case 'schedule':
-                await interaction.message.edit({ embeds: [scheduleembed], components:[gymschedule, gymschedule2], files: [schedulepng] });
-                return;
-	}
-}
-      )
-}
+          await interaction.message.edit({ embeds: [eval(interaction.customId + "embed")], components:[gymschedule, gymschedule2], files: [eval(interaction.customId + "png")]});
+	        }
+        )
+    }
 }
