beb449b87d44f8f915d1a0f3da5e22b24edc86f3
nikist <nikistgar@gmail.com>
2024-05-22 01:28:54 +0300

    devCheck delete

--- source/functions/devCheck.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-module.exports = (client) => {
-    client.devCheck = async () => {
-        eval(`if (interaction.user.id != process.env.OWNER_ID)
-            {
-                return interaction.reply("Команда только для разработчиков")
-            }`)
-    }
-}
