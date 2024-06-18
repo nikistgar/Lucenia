c28d9e10fd95616a49610a979efb5e18441e91d9
nikistgar <nikistgar@gmail.com>
2024-06-18 03:10:28 +0300

    clean

--- source/commands/play.js
+++ source/commands/play.js
@@ -14,27 +14,24 @@ module.exports = {
 	async execute(interaction, client ) {
 		const player = useMainPlayer();
         const channel = interaction.member.voice.channel;
-        if (!channel) return interaction.reply({content: 'You are not connected to a voice channel!', ephemeral: true}); // make sure we have a voice channel
-        const query = interaction.options.getString('query', true); // we need input/query to play
+        if (!channel) return interaction.reply({content: 'You are not connected to a voice channel!', ephemeral: true});
+        const query = interaction.options.getString('query', true);
 
-        // let's defer the interaction as things can take time to process
         await interaction.deferReply();
 
         try {
             const { track } = await player.play(channel, query, {
                 nodeOptions: {
                     skipOnNoStream: true,
-                    // nodeOptions are the options for guild node (aka your queue in simple word)
-                    metadata: interaction, // we can access this metadata object using queue.metadata later on
+                    metadata: interaction,
                     selfDeaf: true,
                     leaveOnEmpty: true,
-                    leaveOnEnd: false
+                    leaveOnEnd: true
                 }
             });
 
             return interaction.followUp(`**${track.cleanTitle}** enqueued!`);
         } catch (e) {
-            // let's return error if something failed
             return interaction.followUp({content: `Something went wrong: ${e}`, ephemeral: true});
         }
     }
